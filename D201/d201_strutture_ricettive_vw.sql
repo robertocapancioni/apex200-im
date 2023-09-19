dcaf310fa32339a5ee9f4e6f5538fa1efd0a4a40
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-09-19 11:01:48 +0200

    Create d201_strutture_ricettive_vw.sql

---
+++ D201/d201_strutture_ricettive_vw.sql
@@ -0,0 +1,23 @@
+create or replace view d201_strutture_ricettive_vw as
+select 
+      sr.ID,
+      sr.DENOMINAZIONE,
+      sr.INDIRIZZO,
+      sr.PRIVATI,
+      sr.LINGUE,
+      sr.SPORT,
+      sr.LATITUDINE,
+      sr.LONGITUDINE,
+      sr.COMUNE_ID,
+       c.COMUNE,
+      sr.ZONA_ID,
+       z.ZONA,
+      sr.CLASSIFICAZIONE_ID,
+       l.CLASSIFICAZIONE
+ from D201_STRUTTURE_RICETTIVE sr
+ join D201_COMUNE c
+   on sr.COMUNE_ID = c.COMUNE_ID
+ join D201_ZONA z
+   on sr.ZONA_ID = z.ZONA_ID
+ join D201_CLASSIFICAZIONE l
+   on sr.CLASSIFICAZIONE_ID = l.CLASSIFICAZIONE_ID
