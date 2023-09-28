27c4016f37dd0b4071b64e51a14c5ec625b58812
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-09-28 09:16:29 +0200

    Create d302 document task.sql

---
+++ D301/d302 document task.sql
@@ -0,0 +1,10 @@
+document_task
+  document_id /nn /fk documento
+  task_name vc32 /nn,
+  outcome_code vc32 /nn,
+  approver vc32 /nn,
+  created_on date /nn
+  updated_on date /nn   
+  comments clob
+
+# settings = { prefix: "D302", semantics: "CHAR", language: "EN", APEX: true }
