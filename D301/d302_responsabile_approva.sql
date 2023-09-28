50e629a4871ea73c400379b740863e363d96cbc1
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-09-28 09:50:28 +0200

    Update d302_responsabile_approva.sql

--- D301/d302_responsabile_approva.sql
+++ D301/d302_responsabile_approva.sql
@@ -1,12 +1,14 @@
 declare
 l_new_task_id  number;
+l_initiator varchar(32);
 begin
 
+  l_initiator := d302_pkg.insert_task_log(:APEX$TASK_ID);
 
   l_new_task_id := apex_approval.create_task(
                          p_application_id => :app_id,
                          p_task_def_static_id => 'CEO',
-                         p_initiator => :RICHIEDENTE, 
+                         p_initiator => l_initiator, 
                          p_parameters => apex_approval.t_task_parameters(
                                                   1 => apex_approval.t_task_parameter(static_id => 'CODICE', string_value => :codice)
                                          ),
@@ -17,4 +19,3 @@ begin
                       set stato = 'CEO'
                     where id = :APEX$TASK_PK;
 end;
-
