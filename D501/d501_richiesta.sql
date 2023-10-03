d3ff3b9acce255fbfaff549fd9f9234d27c5a59c
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-10-03 10:21:09 +0200

    Create d501_richiesta.sql

---
+++ D501/d501_richiesta.sql
@@ -0,0 +1,8 @@
+richiesta
+  codice vc20 /nn /unique
+  descrizione vc50 /nn
+  importo number /nn
+  stato vc20
+
+
+# settings = { prefix: "D501", drop:true, semantics: "CHAR", auditCols: true, language: "EN", APEX: true }
