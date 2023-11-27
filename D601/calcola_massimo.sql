04dbbab730bd079f9c51f1619b8f3d2c3cef0b65
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-11-27 22:22:59 +0100

    Create calcola_massimo.sql

---
+++ D601/calcola_massimo.sql
@@ -0,0 +1,15 @@
+declare
+    l_livello number;
+begin
+    if :IMPORTO < 500 then
+        l_livello := -1;
+    elsif :IMPORTO < 1000 then
+        l_livello := 1;
+    elsif :IMPORTO < 5000 then
+        l_livello := 2;
+    else
+        l_livello := 3;
+    end if;
+    :LIVELLO_MAX := l_livello;
+    :LIVELLO_CORRENTE := 1;
+end;
