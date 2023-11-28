8d167e8cb4d672d91161a044f01e426801ef61c7
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-11-28 19:38:27 +0100

    Update d601_documento_vw.sql

--- D601/d601_documento_vw.sql
+++ D601/d601_documento_vw.sql
@@ -1,17 +1,23 @@
-  create or replace view d601_documento_vw as
-  select r.id,
-         r.codice,
-         r.descrizione,
-         ga.ordine,
-         d.dipartimento,
-         g.gruppo,
-         g.gruppo_des,
-         upper(u.utente)utente,
-         u.nome,
-         u.email
-  from d601_documento r
-  join d601_gruppo_approvatore ga on r.id = ga.documento_id
-  join d601_gruppo g on ga.gruppo_id = g.id
-  join d601_dipartimento d on g.dipartimento_id = d.id
-  join d601_utente_gruppo ug on g.id = ug.gruppo_id
-  join d601_utente u on ug.gruppo_id = u.id
+create or replace view d601_documento_vw as
+select distinct
+            d.id
+          , d.codice
+          , d.descrizione
+          , tibx.sbfl_potential_groups gruppo
+          , case when ga.utente is not null then tibx.link_text else null end as link_text
+          , case when ga.utente is not null  then 'style="display: block"' else 'style="display: none"' end as link_style
+          , inst.prcs_id
+          , tibx.sbfl_id 
+          , tibx.sbfl_step_key step_key
+          --, acld.user_name
+          , tibx.SBFL_CURRENT_NAME Fase
+          , inst.PRCS_INIT_BY
+          , tibx.sbfl_livello_corrente livello_corrente
+     from d601_documento d
+left join flow_instances_vw inst
+       on d.id = to_number(inst.prcs_business_ref default -1 on conversion error) and inst.dgrm_name = 'D601 Documento'
+left join d601_task_inbox_vw tibx
+       on d.id = to_number(tibx.sbfl_business_ref default -1 on conversion error) and tibx.sbfl_dgrm_name = 'D601 Documento'
+left join d601_gruppo_approvatore_vw ga on d.id = ga.documento_id
+                                       and tibx.SBFL_POTENTIAL_GROUPS = ga.gruppo
+                                       and ga.utente = v('APP_USER');
