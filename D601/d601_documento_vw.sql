c07c35f9a78e429c65d39ee6bbdf7e4e6be21ba1
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-11-29 00:51:13 +0100

    Update d601_documento_vw.sql

--- D601/d601_documento_vw.sql
+++ D601/d601_documento_vw.sql
@@ -1,17 +1,18 @@
-create or replace view d601_documento_vw as
-select distinct
+  create or replace force editionable view "D601_DOCUMENTO_VW"  as 
+  select distinct
             d.id
           , d.codice
           , d.descrizione
           , tibx.sbfl_potential_groups gruppo
+          , tibx.sbfl_reservation assegnato
           , case when ga.utente is not null then tibx.link_text else null end as link_text
           , case when ga.utente is not null  then 'style="display: block"' else 'style="display: none"' end as link_style
           , inst.prcs_id
           , tibx.sbfl_id 
           , tibx.sbfl_step_key step_key
           --, acld.user_name
-          , tibx.SBFL_CURRENT_NAME Fase
-          , inst.PRCS_INIT_BY
+          , tibx.sbfl_current_name fase
+          , inst.prcs_init_by
           , tibx.sbfl_livello_corrente livello_corrente
      from d601_documento d
 left join d601_instances_vw inst
@@ -19,5 +20,6 @@ left join d601_instances_vw inst
 left join d601_task_inbox_vw tibx
        on d.id = to_number(tibx.sbfl_business_ref default -1 on conversion error)
 left join d601_gruppo_approvatore_vw ga on d.id = ga.documento_id
-                                       and tibx.SBFL_POTENTIAL_GROUPS = ga.gruppo
+                                       and (tibx.sbfl_potential_groups = ga.gruppo and tibx.sbfl_reservation is null or
+                                            tibx.sbfl_reservation = ga.utente)
                                        and ga.utente = v('APP_USER');
